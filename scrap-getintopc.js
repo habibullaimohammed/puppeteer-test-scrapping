9a3225f916df77afdfed173cea6343dc4ad855d5
Habibullahi Mohammed <habibullaimohammed@gmail.com>
2023-07-11 19:17:37 +0100

    updated index

--- scrap-getintopc.js
+++ scrap-getintopc.js
@@ -1,25 +1,11 @@
-const express = require('express');
 const fs = require('fs');
 const puppeteer = require('puppeteer');
-const cors = require('cors');
-
-const app = express();
-const port =  process.env.PORT || 3000;
-
-app.use(cors()); // Enable CORS for all routes
 
 const TIMEOUT = 30000; // Set the timeout value (in milliseconds) as per your requirements
 const MAX_RETRIES = 3; // Set the maximum number of retries
 
-const categories = [
-    { name: 'authoring', url: 'https://igetintopc.com/category/authoring/' }, // Example Category 1
-    // { name: '2d-designing', url: 'https://igetintopc.com/category/2d-designing/' }, // Example Category 2
-    // { name: 'business-2-software', url: 'https://igetintopc.com/category/business-2-software/' }, // Example Category 3
-    // Add more categories as needed
-];
-
 async function scrapeData(url) {
-    const browser = await puppeteer.launch({ headless: false });
+    const browser = await puppeteer.launch({headless:false});
     const page = await browser.newPage();
     await page.setDefaultTimeout(TIMEOUT); // Set the timeout for each page
 
@@ -47,7 +33,7 @@ async function scrapeData(url) {
                 if (nextButton) {
                     await Promise.all([
                         page.waitForNavigation({ timeout: TIMEOUT }),
-                        nextButton.click(),
+                        nextButton.click()
                     ]);
                 } else {
                     break;
@@ -86,19 +72,22 @@ async function scrapeMultiplePages(categories) {
     return allData;
 }
 
-app.get('/scrapedata', (req, res) => {
-    scrapeMultiplePages(categories)
-        .then((data) => {
-            const json = JSON.stringify(data, null, 2);
-            res.setHeader('Content-Type', 'application/json');
-            res.send(json);
-        })
-        .catch((error) => {
-            console.error(error);
-            res.status(500).send('An error occurred during scraping');
-        });
-});
+const categories = [
+    { name: '3d-analysis', url: 'https://getintopc.com/softwares/3d-analysis/' }, // Example Category 1
+    { name: '3d-animation', url: 'https://getintopc.com/softwares/3d-animation/' }, // Example Category 2
+    { name: '3d-material-rendering', url: 'https://getintopc.com/softwares/3d-material-rendering/' } // Example Category 3
+    // Add more categories as needed
+];
 
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+scrapeMultiplePages(categories).then((data) => {
+    const json = JSON.stringify(data, null, 2);
+    fs.writeFile('all_categories_scraped_data.json', json, (error) => {
+        if (error) {
+            console.error('Error writing to JSON file:', error);
+        } else {
+            console.log('Scraped data for all categories saved to all_categories_scraped_data.json');
+        }
+    });
+}).catch((error) => {
+    console.error(error);
 });
